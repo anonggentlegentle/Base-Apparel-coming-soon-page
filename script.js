da8a4560c77d791025587febd74aca654a671521
metamusk <balberanjarenkim@gmail.com>
2023-02-07 16:53:55 +0800

    Add email js validation

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 "use strict";
 
 const btn = document.querySelector(".btn--submit");
+const field = document.querySelector(".main__field");
+const form = document.querySelector(".main__form");
+const invalidEmail = document.querySelector(".main__form-invalid");
+const label = document.querySelector(".main__label");
+const input = document.querySelector(".main__input");
 
-btn.addEventListener("click", function (e) {});
+const inputValue = input.value;
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault;
+
+  if (!input.validity.valid) {
+    form.classList.add("form-error-border");
+    invalidEmail.classList.add("unhide");
+    label.classList.add("unhide");
+    console.log(form);
+  } else if (input.validity.valid || inputValue === "") {
+    form.classList.remove("form-error-border");
+    invalidEmail.classList.remove("unhide");
+    label.classList.remove("unhide");
+  }
+});
